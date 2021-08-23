a2ba1c1f7c9b740abe24a14c1e097a9f96ce09cc
Ilgiz <lmnasxzz@gmail.com>
2021-08-23 20:17:42 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, {useRef, useState, useCallback, useEffect} from 'react'
-import { compose } from 'redux'
-import './api/api'
 
 import { Gallery } from './components/Gallery/Gallery'
 import './scss/App.scss'
 
  
-// import { IMAGES  } from './api/endpoints'
+ 
 
 function App() {
   return (
     <div className='wrapper'>
         <div className="content">
-          <h1>Task from Ekwid.</h1>
+          <h1>Task from Ecwid.</h1>
           <Gallery/>
         </div>
     </div>
