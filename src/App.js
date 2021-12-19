ade3b2b6ec50febdf2bc3710f9f1b620c4457b98
Ilgiz <lmnasxzz@gmail.com>
2021-12-19 19:41:56 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, {useRef, useState, useCallback, useEffect} from 'react'
 import { Gallery } from './components/Gallery/Gallery'
 import './scss/App.scss'
 
- 
 function App() {
   return (
     <div className='wrapper'>
