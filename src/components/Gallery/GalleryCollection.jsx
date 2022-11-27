b44af2bd6ab0d7bdad230356aec7916e5cb03033
Zagirov Il'giz <lmnasxzz@gmail.com>
2022-11-27 23:31:16 +0300

    Update GalleryCollection.jsx

--- src/components/Gallery/GalleryCollection.jsx
+++ src/components/Gallery/GalleryCollection.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from 'react'
+import React, {useEffect} from 'react'
 import {GalleryItem } from './GalleryItem'
-export const GalleryCollection = ({images,className, removeItemHandler  }) => {
+export const GalleryCollection = ({images,className, removeItemHandler }) => {
     className = className.replace('collection', '') + ''
-    
+ 
        return (
         <React.Fragment>
-            {images.map((item, id)=>{ 
+            {images.map((item, id)=> { 
                   return <GalleryItem  removeItemHandler={removeItemHandler } className={`${className}`} item={item} id={id}/>
-                 })}            
+            })}            
         </React.Fragment>
     )
-
 } 
